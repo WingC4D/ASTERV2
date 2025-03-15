2afb5def68d4827b425b32cacb7d9601233be928
Wing.C4D <mikmuk1@gmail.com>
2025-03-15 09:28:43 +0200

    working 1

--- Astroids/main.py
+++ Astroids/main.py
@@ -10,9 +10,8 @@ def main():
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         screen.fill(000000)
         pygame.display.flip()    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+               return
 if __name__ == "__main__":
     main()
